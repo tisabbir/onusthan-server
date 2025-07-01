2f8feed9ae4dff686afa3a93c5e7811fdc914e51
TI Sabbir <tisabbir0@gmail.com>
2025-07-01 21:01:13 +0600

    cors added

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ const { connectDB } = require('./db/connect');
 const authRoutes = require('./routes/auth');
 const eventRoutes = require('./routes/events');
 const userEventRoutes = require('./routes/userEvents');
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
+
+app.use(cors({
+  origin: ["http://localhost:5173", "https://your-react.vercel.app"],
+  credentials: true
+}));
+
 app.use(express.json());
+
+const PORT = process.env.PORT || 10000;
 
 // Routes
 app.get('/', (req, res) => {
@@ -20,7 +29,7 @@ app.use('/api/myevents', userEventRoutes);
 
 
 connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${process.env.PORT}`);
   });
 });
